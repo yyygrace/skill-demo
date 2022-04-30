39a32bf6d153ae1e4c3ca6a160affc9c8b25be76
Grace <45552424+yyygrace@users.noreply.github.com>
2022-04-29 18:37:58 -0700

    added a test

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,2 +1,5 @@
 public class SkillDemo{
+    public static int multiply(int a, int b){
+        return a*b;
+    }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void multiplication(){
+        assertEquals(2, SkillDemo.multiply(2, 2));
+    }
+}
